53413a8e070f46f81607be20c3a52c1eec118114
Erkan KURNAZ <ekurnaz25@gmail.com>
2026-07-14 15:51:21 +0300

    Phase 4: install Filament v3, FilamentUser auth, fix public/storage symlink

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,15 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -51,5 +53,10 @@ class User extends Authenticatable
     public function roles()
     {
         return $this->belongsToMany(Role::class, 'user_roles', 'user_id', 'role_id');
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->role_id === 1;
     }
 }
