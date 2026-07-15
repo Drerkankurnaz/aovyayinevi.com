341bd6a97bbedc9f9a7fd86b55f37407a69544e1
Erkan KURNAZ <ekurnaz25@gmail.com>
2026-07-15 03:11:42 +0300

    Telif yılını 2026 olarak güncelle ve yazım hatasını düzelt

--- resources/views/vizyonumuz.blade.php
+++ resources/views/vizyonumuz.blade.php
@@ -181,7 +181,7 @@
     <!-- footer -->
     <footer id="footer" class="container-fluid overflow-hidden px-lg-20">
         <div class="copyRightHolder text-center pt-lg-5 pb-lg-4 py-3">
-            <p class="mb-0">Coppyright 2023 by <a href="javascript:void(0);">Otimeta</a> - Tüm hakları saklıdır.</p>
+            <p class="mb-0">Copyright 2026 by <a href="javascript:void(0);">Otimeta</a> - Tüm hakları saklıdır.</p>
         </div>
     </footer>
 </div>
